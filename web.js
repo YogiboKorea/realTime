52f82bc8e6b942354cdc1e63adf38b5834ca40e5
YogiboKorea <yogico.design@gmail.com>
2024-12-06 16:42:16 +0900

    sd

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const cors =require('cors')
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
