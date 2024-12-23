27697d6292831abcf3218e658d5622bba54d0fc5
YogiboKorea <yogico.design@gmail.com>
2024-12-23 16:55:31 +0900

    20

--- web.js
+++ web.js
@@ -139,7 +139,7 @@ async function apiRequest(method, url, data = {}, params = {}) {
 // 최근 등록된 상품 번호 가져오기
 async function getRecentProducts(excludedProductNos = []) {
     try {
-        const limit = 100;
+        const limit = 20;
         const maxProducts = 1000;
         let offset = 0;
         const allProducts = [];
