6887a4738772c9d07930710b2db1b457f00cdc64
YogiboKorea <yogico.design@gmail.com>
2025-02-17 09:44:45 +0900

    real

--- web.js
+++ web.js
@@ -10,8 +10,8 @@ const schedule = require('node-schedule');
 const app = express();
 const PORT = 8014;
 
-let accessToken = 'NPdcwjIVDBykxBPiUNII7B';
-let refreshToken = 'kCgTHFIpCNAgOdUgkKfBIC';
+let accessToken = 'FPgfiY4jICtB9LihPieotD';
+let refreshToken = 'PI5HCvg1CNskfQ4FsW4hjH';
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
